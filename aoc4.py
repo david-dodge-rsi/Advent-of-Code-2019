94f313aecf2d7a1fa55099243a836d5c3ce2c7d0
David Dodge <david.dodge@ruralsourcing.com>
2019-12-02 16:41:47 -0700

    refactor computation to method

--- aoc4.py
+++ aoc4.py
@@ -1,29 +1,34 @@
 def convertToIntArray(inputString):
     return [int(i) for i in inputString.split(",")]
 
-programInput = open("input2.txt")
-memory = convertToIntArray(programInput.read())
+def initializeMemory():
+    programInput = open("input2.txt")
+    return convertToIntArray(programInput.read())
+
+def compute(memory, instuctionLength):
+    for instructionPointer in range(0, len(memory)-1, instructionLength):
+        instruction = memory[instructionPointer : instructionPointer + instructionLength]
+        
+        opcode = instruction[0]
+        parameter1 = instruction[1]
+        parameter2 = instruction[2]
+        parameter3 = instruction[3]
+
+        value1 = memory[parameter1]
+        value2 = memory[parameter2]
+
+        if opcode == 1:
+            result = value1 + value2
+            memory[parameter3] = result
+        elif opcode == 2:
+            result = value1 * value2
+            memory[parameter3] = result
+        elif opcode == 99:
+            break
+    return memory[0]
 
 instructionLength = 4
+memory = initializeMemory()
+result = compute(memory, instructionLength)
 
-for instructionPointer in range(0, len(memory)-1, instructionLength):
-    instruction = memory[instructionPointer : instructionPointer + instructionLength]
-    
-    opcode = instruction[0]
-    parameter1 = instruction[1]
-    parameter2 = instruction[2]
-    parameter3 = instruction[3]
-
-    value1 = memory[parameter1]
-    value2 = memory[parameter2]
-
-    if opcode == 1:
-        result = value1 + value2
-        memory[parameter3] = result
-    elif opcode == 2:
-        result = value1 * value2
-        memory[parameter3] = result
-    elif opcode == 99:
-        break
-
-print(memory[0])
+print(result)
